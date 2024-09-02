1486e9d55c1f9ba1ea8d951ac74b36cc6127b35d
Fahleh <eddyukpong@gmail.com>
2024-09-02 16:43:45 +0100

    Fixes

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 import express from 'express';
+import startServer from './services/boot';
 import injectRoutes from './routes';
-import loadEnv from '../utils/loadEnv';
 
 const server = express();
 
 server.use(express.json({ limit: '200mb' }));
 injectRoutes(server);
-loadEnv();
-const port = process.env.PORT || 5000;
-const env = process.env.npm_lifecycle_event || 'dev';
-server.listen(port, () => {
-  console.log(`[${env}] API has started listening at port:${port}`);
-});
+startServer(server);
 
 export default server;
