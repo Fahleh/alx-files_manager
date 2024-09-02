a5062e7aa499659c6eb12de0a071550930098c0a
Fahleh <eddyukpong@gmail.com>
2024-09-02 15:30:29 +0100

    Fixes

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import startServer from './libs/boot';
 import injectRoutes from './routes';
-import injectMiddlewares from './libs/middlewares';
 
 const server = express();
 
-injectMiddlewares(server);
+server.use(express.json({ limit: '200mb' }));
 injectRoutes(server);
 startServer(server);
 
