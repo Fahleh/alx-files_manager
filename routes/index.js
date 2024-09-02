b547e13b00d94a2771fa97882cdd63b5d86ce947
Fahleh <eddyukpong@gmail.com>
2024-09-02 15:38:29 +0100

    Fixes

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
 import FilesController from '../controllers/FilesController';
-import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
-import { APIError, errorResponse } from '../middlewares/error';
+import { basicAuthenticate, xTokenAuthenticate } from '../services/auth';
+import { APIError, errorResponse } from '../services/error';
 
 /**
  * Injects routes with their handlers to the given Express application.
